1edb5964d434072a92657487b2563aef4b146a3c
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-03-07 00:26:56 +0300

    React 2

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,14 +1,13 @@
 import { useParams, useNavigate } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
   const recipe = useRecipeStore((state) =>
     state.recipes.find((recipe) => recipe.id === Number(id))
   );
-  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   if (!recipe) return <p>Recipe not found</p>;
 
@@ -17,14 +16,7 @@ const RecipeDetails = () => {
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
       <EditRecipeForm recipe={recipe} />
-      <button
-        onClick={() => {
-          deleteRecipe(recipe.id);
-          navigate("/");
-        }}
-      >
-        Delete Recipe
-      </button>
+      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
 };
