613be9bab4bcab14d6c1770dde7e76fe41119a9d
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-03-17 16:04:53 +0300

    Third commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="max-w-xs p-4 mx-auto my-20 text-center bg-gray-100 rounded-lg shadow-lg sm:p-8 sm:max-w-sm">
+    <div className="max-w-xs mx-auto my-20 text-center bg-gray-100 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
