63aea04f0bd0e56917e742e2f377e317804e12e6
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-03-30 23:55:10 +0300

    fourth commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,3 +10,17 @@ export const fetchUserData = async (username) => {
     return null;
   }
 };
+
+export const fetchAdvancedUserSearch = async (query, location, minRepos) => {
+  try {
+    const searchQuery = `q=${query}${location ? `+location:${location}` : ""}${
+      minRepos ? `+repos:>${minRepos}` : ""
+    }`;
+    const response = await axios.get(
+      `https://api.github.com/search/users?${searchQuery}`
+    );
+    return response.data.items;
+  } catch (error) {
+    return [];
+  }
+};
